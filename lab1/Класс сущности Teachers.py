feaca90e98d78da90799ef6672eb013bb210d41a
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2024-09-15 19:47:52 +0300

    Add files via upload
    Добавлена валидация полей класса

--- lab1/Класс сущности Teachers.py
+++ lab1/Класс сущности Teachers.py
@@ -1,7 +1,68 @@
+import phonenumbers
+
 #класс преподавателей
 
 class Teacher:
     def __init__(self, teacher_id, name, surname, patronymic, phone, work_experience, department, group_id):
+        self.__teacher_id = teacher_id
+        self.__name = name
+        self.__surname = surname
+        self.__patronymic = patronymic
+        self.__phone = phone
+        self.__work_experience = work_experience
+        self.__department = department
+        self.__group_id = group_id
+
+    @staticmethod
+    def val_teacher_id(teacher_id):
+        if not isinstance(teacher_id, int) or teacher_id <= 0:
+            raise ValueError("")
+
+    @staticmethod
+    def val_name(name):
+        if not isinstance(name, str) or not name.strip():
+            raise ValueError("Ошибка ввода имени")
+
+    @staticmethod
+    def val_surname(surname):
+        if not isinstance(surname, str) or not surname.strip():
+            raise ValueError("Ошибка ввода фамилии")
+
+    @staticmethod
+    def val_patronymic(patronymic):
+        if not isinstance(patronymic, str) or not patronymic.strip():
+            raise ValueError("Ошибка ввода  отчества ")
+
+    @staticmethod
+    def val_phone(phone):
+        if not (phone[0]=="+" and phone[1:].isdigit() and len(phone)==12):
+            raise ValueError("Ошибка ввода телефона ")
+
+    @staticmethod
+    def val_work_experience(work_experience):
+        if not isinstance(work_experience, int) or work_experience < 0:
+            raise ValueError("Ошибка ввода стажа")
+
+    @staticmethod
+    def val_department(department):
+        if not isinstance(department, str) or not department.strip():
+            raise ValueError("Ошибка ввода отделения")
+
+    @staticmethod
+    def val_group_id(group_id):
+        if not isinstance(group_id, int) or group_id <= 0:
+            raise ValueError("Ошибка ввода id группы")
+
+    def __init__(self, teacher_id, name, surname, patronymic, phone, work_experience, department, group_id):
+        self.val_teacher_id(teacher_id)
+        self.val_name(name)
+        self.val_surname(surname)
+        self.val_patronymic(patronymic)
+        self.val_phone(phone)
+        self.val_work_experience(work_experience)
+        self.val_department(department)
+        self.val_group_id(group_id)
+
         self.__teacher_id = teacher_id
         self.__name = name
         self.__surname = surname
@@ -23,6 +84,7 @@ class Teacher:
 
     @name.setter
     def name(self, value):
+        self.val_name(value)
         self.__name = value
 
     @property
@@ -31,6 +93,7 @@ class Teacher:
 
     @surname.setter
     def surname(self, value):
+        self.val_surname(value)
         self.__surname = value
 
     @property
@@ -39,6 +102,7 @@ class Teacher:
 
     @patronymic.setter
     def patronymic(self, value):
+        self.val_patronymic(value)
         self.__patronymic = value
 
     @property
@@ -47,6 +111,7 @@ class Teacher:
 
     @phone.setter
     def phone(self, value):
+        self.val_phone(value)
         self.__phone = value
 
     @property
@@ -55,6 +120,7 @@ class Teacher:
 
     @work_experience.setter
     def work_experience(self, value):
+        self.val_work_experience(value)
         self.__work_experience = value
 
     @property
@@ -63,6 +129,7 @@ class Teacher:
 
     @department.setter
     def department(self, value):
+        self.val_department(value)
         self.__department = value
 
     @property
@@ -71,12 +138,12 @@ class Teacher:
 
     @group_id.setter
     def group_id(self, value):
+        self.val_group_id(value)
         self.__group_id = value
 
     def __str__(self):
         return f"Teacher ID: {self.teacher_id}, Name: {self.name}, Surname: {self.surname}, Patronymic: {self.patronymic}, Phone: {self.phone}, Work Experience: {self.work_experience}, Department: {self.department}, Group ID: {self.group_id}"
-
-teacher = Teacher(1, "John", "Doe", "Junior", "123-456-7890", 5, "Math", 101)
+teacher = Teacher(1, "John", "Doe", "Junior", "+79999999911", 5, "Math", 101)
 print(teacher.name)
 
 teacher.name = "Jane"
